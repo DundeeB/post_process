5e4a411fba40b0e684d505f00342f7eaabecd3c9
Daniel <ligolas@gmail.com>
2019-09-06 11:01:55 +0300

    Fixed same bug for S_Bragg for Sm_Bragg

--- calc_Sm_Bragg_for_lib.m
+++ calc_Sm_Bragg_for_lib.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function [S] = calc_Sm_Bragg_for_lib(lib, k, z0)
 dir_struct = dir(lib);
 homeDir = pwd;
@@ -8,41 +7,19 @@ cd(lib);
 N_converge = length(dir_struct);
 S = zeros(N_converge + 1, Nk);
 j = 1;
+I = [];
 for i=1:length(dir_struct)
     fold = dir_struct(i).name;
     try
         spheres = dlmread(fold);
         j = j+1;
+        I(end+1) = i;
         S(j,:) = S(j-1,:) + Sm_Bragg(k, spheres, z0)';
     catch err
         disp([fold ' is not spheres data folder']);
-        disp(err.message)
     end
 end
-S = S(1:j,:)./(1:j)';
-cd(homeDir);
-end
-=======
-function [S] = calc_Sm_Bragg_for_lib(lib, k, z0)
-dir_struct = dir(lib);
-homeDir = pwd;
-addpath(homeDir);
-cd(lib);
-[~, Nk] = size(k);
-N_converge = length(dir_struct);
-S = zeros(N_converge + 1, Nk);
-j = 1;
-for i=1:length(dir_struct)
-    fold = dir_struct(i).name;
-    try
-        spheres = dlmread(fold);
-        j = j+1;
-        S(j,:) = S(j-1,:) + Sm_Bragg(k, spheres, z0)';
-    catch err
-        disp([fold ' is not spheres data folder']);
-        disp(err.message)
-    end
-end
-S = S(1:j,:)./(1:j)';
+norm = (1:length(I))'.^-1*ones(1,Nk);
+S = S(I,:).*norm;
 cd(homeDir);
 end
