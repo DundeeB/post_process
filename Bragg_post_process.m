75e6ecfbb39956e3fe1b00d22e3409605937f9ae
Daniel Abutbul <ligolas@gmail.com>
2019-09-10 10:08:02 +0300

    All kind of post procceing are running. Commit before making the big change of organizing everything into post proccess

--- Bragg_post_process.m
+++ Bragg_post_process.m
@@ -4,7 +4,8 @@ h = 0.6;
 rhoH = 0.4;
 rad = 1; sig = 2*rad;
 a = sig/sqrt(rhoH*(h+1));
-lib = ['simulation-results\N=' num2str(N) '_h=' num2str(h) '_rhoH=' num2str(rhoH)];
+lib = ['simulation-results\N=' num2str(N) '_h=' num2str(h), '_rhoH=' num2str(rho_H) ...
+        '_triang'];
 %% kx Bragg picks at 2pi
 k=[];
 kx_max = 3.5*pi/a;
@@ -24,8 +25,8 @@ toc
 tic
 kxy = k(1:2,:);
 z0 = sig*(1+h)/2;
-Sm_convergence = calc_Sm_Bragg_for_lib(lib,kxy, z0);
-Sm = abs(Sm_convergence(end,:));
+Sm_convergence_45 = calc_Sm_Bragg_for_lib(lib,kxy, z0);
+Sm_45 = abs(Sm_convergence_45(end,:));
 toc
 
 %% plot all
@@ -35,8 +36,8 @@ figure;
 plot(kx*a/pi,S,'o--');
 hold all;
 plot(2+0*minmax(S),[0 max(S)],'--Black','LineWidth',3);
-plot(kx*a/pi,Sm,'o--');
-plot(1+0*minmax(S),[0 max(Sm)],'--m','LineWidth',3);
+plot(kx*a/pi,Sm_45,'o--');
+plot(1+0*minmax(S),[0 max(Sm_45)],'--m','LineWidth',3);
 
 set(gca,'FontSize',24);
 xlabel('k [\pi/a]');
@@ -49,19 +50,19 @@ legend('<|S|^2>=<|1/N\Sigma_ie^{ikr}|^2>','(2\pi, 2\pi)',...
 
 %% S magnetic pi pi pick dependence on theta
 tic
-theta = 0:(2*pi/16):2*pi;
+theta = 0:pi/64:pi/2;
 rad = 1;
 a = 2*rad/sqrt(rhoH*(h+1));
 kxy = 2/sqrt(2)*pi/a*[cos(theta); sin(theta)];
 z0 = sig*(1+h)/2;
 %% calculate
-Sm_convergence = calc_Sm_Bragg_for_lib(lib,kxy, z0);
-Sm = abs(Sm_convergence(end,:));
+Sm_convergence_theta = calc_Sm_Bragg_for_lib(lib,kxy, z0);
+Sm_theta = abs(Sm_convergence_theta(end,:));
 toc
 %% plot theta dependence
 figure;
 
-plot(theta/pi*180,Sm,'o--'); xlim([0 360]);ylim([0 max(Sm)]);
+plot(theta/pi*180,Sm_theta,'o--'); xlim([0 360]);ylim([0 max(Sm_theta)]);
 
 set(gca,'FontSize',24);
 xlabel('\theta');
@@ -70,3 +71,14 @@ title(['Bragg scattering \theta dependence, N=' num2str(N) ' h=' num2str(h) ...
     ' \rho_H=' num2str(rhoH)]);grid on; 
 legend('<|z(k)|^2>=<|1/N\Sigma_iz_ie^{ikr}|^2> at |k|=2^{1/2}\pi',...
     'Location','NorthWest');
+%% plot theta convergence
+figure;
+
+hold all;plot(Sm_convergence_theta);
+
+set(gca,'FontSize',24);
+xlabel('# realizations sumed into S_m');
+ylabel('|S_m|^2');
+title(['Bragg scattering convergence from triang, N=' num2str(N) ' h=' num2str(h) ...
+    ' \rho_H=' num2str(rhoH)]);grid on; 
+save([lib '\Bragg_post_process'],'Sm_convergence_theta','S_convergence','Sm_convergence_45');
