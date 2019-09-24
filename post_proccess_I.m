171ee574a4cce8428b496601d576467e2c85103a
Daniel Abutbul <ligolas@gmail.com>
2019-09-24 17:15:13 +0300

    Going to Holydays!

---
+++ post_proccess_I.m
@@ -0,0 +1,28 @@
+father_dir = 'simulation-results\';
+folds_obj = dir(father_dir);
+sim_dirs = {};
+for i=1:length(folds_obj)
+    f = folds_obj(i).name;
+    if sum(strcmp(f,{'.','..','Small or 2D simulations'}))||...
+            ~isdir([father_dir f])  
+        continue
+    end
+    sim_dirs{end+1} = ['simulation-results\' f];
+end
+n = length(sim_dirs);
+rho_H_vec = zeros(n,1);
+h_vec = rho_H_vec;
+N_vec = rho_H_vec;
+
+for i=1:n
+    rho_H_vec(i) = str2double(regexprep(regexprep(...
+        sim_dirs{i},'.*rhoH=',''),'_.*',''));
+    N_vec(i) = str2double(regexprep(regexprep(...
+        sim_dirs{i},'_h=.*',''),'.*N=',''));
+    h_vec(i) = str2double(regexprep(regexprep(...
+        sim_dirs{i},'.*h=',''),'_rhoH.*',''));
+
+    if h_vec(i) == 0.7 && N_vec(i) == 900 && rho_H_vec(i) >= 0.4
+        post_process(sim_dirs{i},true,'output',50);
+    end
+end
