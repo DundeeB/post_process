88f335a2e3f2cc8f6937f02bfad3d0e39393a6e2
Daniel Abutbul <ligolas@gmail.com>
2019-11-12 14:44:06 +0200

    Before changing the lines I plot in plot_for_all_simulation

--- plot_for_all_simulations.m
+++ plot_for_all_simulations.m
@@ -10,11 +10,12 @@ for i=1:length(folds_obj)
     sim_dirs{end+1} = ['simulation-results\' f];
 end
 n = length(sim_dirs);
-rho_H_vec = zeros(n,1);
+rho_H_vec = zeros(n,1)*nan;
 h_vec = rho_H_vec;
 N_vec = rho_H_vec;
 psi_vec = rho_H_vec;
 Sm_pi_pi = rho_H_vec;
+b1_vec = rho_H_vec;
 for i=1:n
     rho_H_vec(i) = str2double(regexprep(regexprep(...
         sim_dirs{i},'.*rhoH=',''),'_.*',''));
@@ -26,51 +27,87 @@ for i=1:n
     cd(sim_dirs{i});
     try
         load('output.mat');
-        I = find(theta*180/pi==45,1,'first');
-        Sm_pi_pi(i) = Sm_theta(end-50,I);
+        I1 = find(theta*180/pi==45,1,'first');
+        Sm_pi_pi(i) = Sm_theta(end-50,I1)*N_vec(i);
         try
             load('correct_psi_100_real');
+            psi_vec(i) = psi14(end);
         end
-        psi_vec(i) = psi14(end);
+        try
+            load('M_fr_100_real');
+            b1_vec(i) = b(end);
+        end
     catch err
         disp(err.message)
     end
     cd('../../');
 end
 %%
+I1 = N_vec == 100;
+I2 = N_vec == 400;
+I3 = N_vec == 900;
+I4 = N_vec == 3600;
+figure; hold all;
+plt = @(I)plot(rho_H_vec(I), h_vec(I),'.','MarkerSize',15); 
+plt(I1);plt(I2);plt(I3);plt(I4);
+legend('100','400','900','3600');
+xlim([0 1.15]); ylim([0 1]);
+xlabel('\rho_H');ylabel('h');
+set(gca,'FontSize',20);
+%%
 h=figure; 
 
 subplot(2,1,1); hold all;
-I = h_vec==0.7 & N_vec==3600;
+I1 = h_vec==0.7 & N_vec==3600;
 I2 = h_vec==0.7 & N_vec==900;
-plot(rho_H_vec(I),(psi_vec(I)),'.--','MarkerSize',30);
-plot(rho_H_vec(I2),(psi_vec(I2)),'.-','MarkerSize',10);
+plot(rho_H_vec(I1),(psi_vec(I1)),'.-','MarkerSize',20);
+plot(rho_H_vec(I2),(psi_vec(I2)),'.-','MarkerSize',20);
 xlabel('\rho_H'); 
 legend('h=0.7, N=3600','h=0.7, N=900','Location','NorthWest');
 ylabel('|\psi_{14}|');
 set(gca,'FontSize',20); grid on;
 
 subplot(2,1,2); hold all;
-I = rho_H_vec==0.4 & N_vec==3600;
+I1 = rho_H_vec==0.4 & N_vec==3600;
 I2 = rho_H_vec==0.4 & N_vec==900;
 I3 = rho_H_vec==0.4 & N_vec==400;
 I4 = rho_H_vec==0.4 & N_vec==100;
-plot(h_vec(I),Sm_pi_pi(I),'.--','MarkerSize',30);
-plot(h_vec(I2),Sm_pi_pi(I2),'.-','MarkerSize',10);
-plot(h_vec(I3),Sm_pi_pi(I3),'.-','MarkerSize',10);
+plot(h_vec(I1),Sm_pi_pi(I1),'.-','MarkerSize',20);
+plot(h_vec(I2),Sm_pi_pi(I2),'.-','MarkerSize',20);
+plot(h_vec(I3),Sm_pi_pi(I3),'.-','MarkerSize',20);
 xlabel('h');
-grid on;title('\rho_H=0.4');
-legend('N=3600','N=900','N=400','N=100','Location','NorthWest');
+grid on;
+legend('\rho_H=0.4, N=3600','\rho_H=0.4, N=900','\rho_H=0.4, N=400','\rho_H=0.4, N=100','Location','NorthWest');
 ylabel('<|z(k=(\pi,\pi))/(h\sigma)|^2>');
 set(gca,'FontSize',20);
 
-savefig(h,'simulation-results\magnetic_Bragg_vs_h');
+savefig(h,'graphs\magnetic_Bragg_vs_h');
 %%
-g=figure; 
-I = h_vec==0.7 & N_vec==900;
-plot(rho_H_vec(I),(psi_vec(I)),'.--','MarkerSize',30);
-legend('N=900, h=0.7','Location','NorthWest');
-ylabel('|\psi_{14}|');
-grid on;
-set(gca,'FontSize',20);
+j=figure; 
+
+subplot(2,1,1); hold all;
+I1 = h_vec==0.7 & N_vec==3600;
+I2 = h_vec==0.7 & N_vec==900;
+plot(rho_H_vec(I1),(psi_vec(I1)),'.-','MarkerSize',20);
+plot(rho_H_vec(I2),(psi_vec(I2)),'.-','MarkerSize',20);
 xlabel('\rho_H'); 
+legend('h=0.7, N=3600','h=0.7, N=900','Location','NorthWest');
+ylabel('|\psi_{14}|');
+set(gca,'FontSize',20); grid on;
+
+subplot(2,1,2); hold all;
+I1 = rho_H_vec==0.4 & N_vec==3600;
+I2 = rho_H_vec==0.4 & N_vec==900;
+I3 = rho_H_vec==0.4 & N_vec==400;
+I4 = rho_H_vec==0.4 & N_vec==100;
+plot(h_vec(I1),b1_vec(I1),'.-','MarkerSize',20);
+plot(h_vec(I2),b1_vec(I2),'.-','MarkerSize',20);
+plot(h_vec(I3),b1_vec(I3),'.-','MarkerSize',20);
+plot(h_vec(I4),b1_vec(I4),'.-','MarkerSize',20);
+xlabel('h');
+grid on;
+legend('\rho_H=0.4, N=3600','\rho_H=0.4, N=900','\rho_H=0.4, N=400','\rho_H=0.4, N=100','Location','NorthWest');
+ylabel('b_1');
+set(gca,'FontSize',20);
+
+savefig(j,'graphs\magnetic_Bragg_vs_h');
