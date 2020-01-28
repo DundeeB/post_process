69e7fea71bbaa3a555dda373372fa6250fec1423
Daniel Abutbul <ligolas@gmail.com>
2020-01-28 09:47:44 +0200

    Cleaned the code a bit

--- calc_psi_param_for_lib.m
+++ calc_psi_param_for_lib.m
@@ -1,16 +1,18 @@
 function psi_global = calc_psi_param_for_lib(lib, m, n, N_realizations)
 load_lib;
-psi_global = [0];
 if nargin == 3 || N_realizations > N_sph_files
     N_max = N_sph_files;
 else
     N_max = N_realizations;
 end
+psi_global = zeros(N_max+1,1);
+j = 1;
 for i=N_sph_files:-1:N_sph_files-N_max+1
     spheres = dlmread(files{i});
     [psi_local, ~] = psi_mn(m, n, spheres, state.cyclic_boundary);
-    psi_global(end+1) = psi_global(end) + abs(mean(psi_local));
+    psi_global(j+1) = psi_global(j) + abs(mean(psi_local));
+    j = j+1;
 end
-psi_global = psi_global(2:end)./[1:length(psi_global)-1];
+psi_global = psi_global(2:end)./(1:(length(psi_global)-1));
 cd(homeDir);
 end
--- psi_mn.m
+++ psi_mn.m
@@ -1,16 +1,20 @@
 function [psimn_vec, E] = psi_mn(m, n, spheres, cyclic_boundary)
-%PSI_N find for the i'th sphere it's nearest neighbors and calculate the
-%psi_n order parameter for the i'th sphere
-%psi(m,n) is the order parameter 
+%psi_mn = abs(psi_n)*exp(i*m*angle(psi_n)) where
+%psi_n = sum_{nearest neighbors} exp(i*n*t) and t is the angle of the bond
+%between the sphere and its nearest neighbor. psimn_vec is a vector where
+%for the i'th sphere its local psi_mn is psimn_vec(i). The number of
+%nearest neighbors is n, and if they are oriented in 2pi/n then psi_n will
+%be 1. If there are N>n equivelent angle, then m=N/n. For example, in the
+%honeycomb lattice there are N=6 equivelent angles, and each sphere has n=3
+%nearest neighbors, and so m=2, and psi_23 is the correct order parameter.
+%For triangular lattice N=n=6 and m=1, and for square N=n=4 and m=1.
 psimn_vec = zeros(length(spheres),1);
 E = knn_based_bonds(spheres, n, cyclic_boundary);
 psi_n = zeros(size(psimn_vec));
 Neighbors = zeros(size(psimn_vec));
 for i=1:length(E)
-    a = E(i,1);
-    r1 = spheres(a,:);
-    b = E(i,2);
-    r2 = spheres(b,:);
+    a = E(i,1); b = E(i,2);
+    r1 = spheres(a,:); r2 = spheres(b,:);
     dr = r1 - r2;
     t = atan2(dr(2),dr(1));
     psi_n(a) = psi_n(a) + exp(1i*n*t);
@@ -18,7 +22,5 @@ for i=1:length(E)
 end
 I = Neighbors~=0;
 psi_n(I) = psi_n(I)./Neighbors(I);
-for i=1:length(psi_n)
-    psimn_vec(i) = abs(psi_n(i))*exp(1i*m*angle(psi_n(i)));
-end
+psimn_vec = abs(psi_n).*exp(1i*m*angle(psi_n));
 end
