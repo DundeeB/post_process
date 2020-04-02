4cf1756738cbee7aa1e5827dee8f14f6b68030f1
DundeeB <ligolas@gmail.com>
2020-04-02 11:32:58 +0300

    Before making color_map density by boxes instead of plotting local density around each sphere (current version)

--- color_map.m
+++ color_map.m
@@ -3,11 +3,11 @@ function [] = color_map(state, psi)
 sp = state.spheres/(2*state.rad);
 state.cyclic_boundary = state.cyclic_boundary/(2*state.rad);
 Heaveside = @(x) x.*(x>0);
-rad_avg = 3;
+rad_avg = 5;
 
 figure; 
 
-subplot(2,1,1); 
+subplot(2,2,1); 
 hold on;
 for i=1:length(sp)
     dr = zeros(size(psi));
@@ -23,12 +23,19 @@ end
 axis equal;
 L = max(state.cyclic_boundary);
 xlim([0 L]); ylim([0 L]);
-colorbar('Ticks',[0,0.5,1],...
+colorbar('northoutside','Ticks',[0,0.5,1],...
          'TickLabels',{'<real(\psi)>=-1','|<\psi>|=0','<real(\psi)>=1'})
 colormap('jet');
 xlabel('x/\sigma');ylabel('y/\sigma');
 
-subplot(2,1,2);
+subplot(2,2,3); hold on; 
+[counts,centers] = hist(abs(psi));
+plot(centers,counts/trapz(centers,counts),'o-','LineWidth',3);
+xlabel('|\psi|'); ylabel('pdf');
+% set(gca,'FontSize',20); 
+grid on;
+
+subplot(2,2,2);
 hold on;
 rho = zeros(size(psi));
 for i=1:length(sp)
@@ -39,16 +46,17 @@ for i=1:length(sp)
     I = dr<rad_avg;
     rho(i) = sum(I)/(pi*rad_avg^2*state.H/(2*state.rad));
 end
-rho_red = min(rho); rho_blue = max(rho);
+rho_green = min(rho); rho_blue = max(rho);
 for i=1:length(sp)
-    RGB = Heaveside([1-(rho(i)-rho_red)/(rho_blue-rho_red) 0 1-(rho(i)-rho_blue)/(rho_red-rho_blue)]);
+    RGB = Heaveside([0 1-(rho(i)-rho_green)/(rho_blue-rho_green) 1-(rho(i)-rho_blue)/(rho_green-rho_blue)]);
     plot(sp(i,1),sp(i,2),'.','Color',RGB,'MarkerSize',7);
 end
 axis equal;
 L = max(state.cyclic_boundary);
 xlim([0 L]); ylim([0 L]);
-colorbar('Ticks',[0,1],...
-         'TickLabels',{['\rho_H=' num2str(rho_blue)],['\rho_H=' num2str(rho_red)]})
+c=colorbar('northoutside','Ticks',[0,0.5],...
+         'TickLabels',{['\rho_H=' num2str(rho_blue)],['\rho_H=' num2str(rho_green)]});
 colormap('jet');
+c.Limits = [ 0 0.5];
 xlabel('x/\sigma');ylabel('y/\sigma');
 end
--- zz_script.m
+++ zz_script.m
@@ -2,20 +2,20 @@ figure; hold all;
 
 load from_ATLAS\N=3600_h=1.0_rhoH=0.93_AF_triangle_ECMC\'state 17992800.mat';
 [counts, centers] = zz_correlation(state);
-plot(centers/2,counts/(1*2/2),'--');
+plot(centers/2,counts/(1*2/2),'--','LineWidth',3);
 
 load from_ATLAS\N=3600_h=1.0_rhoH=0.85_AF_triangle_ECMC\'state 17992800.mat'
 [counts, centers] = zz_correlation(state);
-plot(centers/2,counts/(1*2/2),'--');
+plot(centers/2,counts/(1*2/2),'--','LineWidth',3);
 
 load from_ATLAS\N=3600_h=1.0_rhoH=0.55_AF_square_ECMC\'state 17992800.mat'
 [counts, centers] = zz_correlation(state);
-plot(centers/2,counts/(1*2/2),'--');
+plot(centers/2,counts/(1*2/2),'--','LineWidth',3);
 
 load ../simulation-results/N=3600_h=1_rhoH=0.1_AF_triangle/Input_parameters.mat
 state.spheres = dlmread('../simulation-results/N=3600_h=1_rhoH=0.1_AF_triangle/20000000');
 [counts, centers] = zz_correlation(state);
-plot(centers/2,counts/(1*2/2),'--');
+plot(centers/2,counts/(1*2/2),'--','LineWidth',3);
 
 plot(centers/2, centers*0+0.25,'-k','LineWidth',2);
 %%
@@ -24,4 +24,4 @@ legend('\rho_H=0.93','\rho_H=0.85','\rho_H=0.5','\rho_H=0.1',...
 grid on; set(gca,'FontSize', 25);
 xlabel('\Deltar/\sigma');
 ylabel('<|zz|>/(h\sigma/2)');
-% xlim([0 15]); ylim([0 1]);
+xlim([0 15]); ylim([0 1]);
